4ffc4b0f604f06c45be2512f57d6402e3e2197d5
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2022-12-22 19:20:18 +0800

    updated to support reading from memory bytes

--- fitz_utils/ProcessedDoc.py
+++ fitz_utils/ProcessedDoc.py
@@ -7,8 +7,11 @@ from .ProcessedPage import ProcessedPage
 class ProcessedDoc:
     """Class to provide extra methods to pymupdf doc class"""
 
-    def __init__(self, fname: Path) -> None:
-        self.doc = fitz.open(str(fname))
+    def __init__(self, fname: Path = None, stream: bytes = None) -> None:
+        if not fname:
+            self.doc = fitz.open(stream=stream)
+        else:
+            self.doc = fitz.open(str(fname))
         pass
 
     def __len__(self) -> int:
